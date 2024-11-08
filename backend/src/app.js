0c14b687a55fa7faffc113f567880c8785ea3aa4
Paramveer Singh <paramveersingh131203@gmail.com>
2024-11-08 18:24:13 +0530

    Implement user authentication with JWT, including sign-up and sign-in routes, error handling, and database connection setup

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+import userRouter from "./routes/user.routes.js";
+app.use("/api/auth", userRouter);
+
+export { app };
