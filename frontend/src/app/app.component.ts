701624c06e266f7d1edd6e60e9fefb1a80102ae1
Atharv7740 <atharv7740@gmail.com>
2024-11-09 10:54:28 +0530

    signup component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
+
 import { SignupComponent } from './signup/signup.component'; // Import SignupComponent
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="container">
-      <!-- <app-login></app-login>  -->
+      
       <app-signup></app-signup> <!-- Display SignupComponent -->
     </div>
   `,
