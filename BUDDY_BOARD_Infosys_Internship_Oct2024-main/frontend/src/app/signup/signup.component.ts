c14c0e83964c1dfbe936e6ec464363b262a70c0a
Ishratnoori <ishratnoori18@gmail.com>
2024-11-09 15:54:38 +0530

    Added some validation

---
+++ BUDDY_BOARD_Infosys_Internship_Oct2024-main/frontend/src/app/signup/signup.component.ts
@@ -0,0 +1,12 @@
+// src/app/signup/signup.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+  standalone: true,
+})
+export class SignupComponent {
+  title = 'Sign Up';
+}
